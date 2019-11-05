3ca46f269add25a21c69ff83c1a4dfa61d18e40d
kyeah01 <kyeah01@gmail.com>
2019-11-05 19:29:32 +0900

    191105 | 개발환경 / 프로덕션 환경 분할을 위한 settings.py 분할 - local setting : sqlite3, django secret key가 무작위로 변경되도록 설정, debug True - develop setting : mysql, debug True - production : mysql, debug False

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -15,37 +15,10 @@ from django.core.exceptions import ImproperlyConfigured
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-secret_file = os.path.join(BASE_DIR, 'secret.json')
-
-with open(secret_file) as f:
-    secrets = json.loads(f.read())
-
-def get_secret(setting, secrets=secrets):
-    """비밀 변수를 가져오거나 명시적 예외를 반환한다."""
-    try:
-        return secrets[setting]
-    except KeyError:
-        error_msg = "Set the {} environment variable".format(setting)
-        raise ImproperlyConfigured(error_msg)
-
-DJANGO_SECRET_KEY = get_secret("DJANGO_SECRET")
-MYSQL_SECRET_KEY = get_secret("MYSQL_SECRET")
-
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = DJANGO_SECRET_KEY
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
@@ -101,31 +74,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'backend.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-# original - sqlite3
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-# MySQL
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'apiwikiproject',
-        'USER': 'apiwiki',
-        'PASSWORD': MYSQL_SECRET_KEY,
-        'HOST': '192.168.31.52',
-        'PORT': '3306',
-    }
-}
-
 
 
 # django log
---
+++ backend/backend/settings/local.py
@@ -0,0 +1,16 @@
+import os
+import string, random
+from backend.settings.base import *
+
+DEBUG = True
+ALLOWED_HOSTS = []
+chars = ''.join([string.ascii_letters, string.digits, string.punctuation]).replace('\'', '').replace('"', '').replace('\\', '')
+
+SECRET_KEY = ''.join([random.SystemRandom().choice(chars) for i in range(50)])
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
